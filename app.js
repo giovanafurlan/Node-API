3ad755b56f57adb7f52826af014f49d3d4b3575e
giovanafurlan <giovananelofurlan@gmail.com>
2022-07-28 00:14:45 -0300

    Teste

--- app.js
+++ app.js
@@ -1,23 +1,74 @@
-const express = require('express'); //importacao do pacote
-const app = express(); //instanciando express
-const cors = require('cors');
-const bodyParser = require('body-parser'); // middleware
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
 
-// Route to Login Page
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(express.static("public"));
+
+// Connecting gfg-employees database to our express application
+
+mongoose.connect(
+"mongodb://localhost:27017/gfg-employees",
+{ useNewUrlParser: true }
+);
+
+// Writing schema for employee-data collection
+const employeeSchema = {
+	employee_name: String,
+	employee_department: String,
+	employee_salary: Number
+};
+
+// Creating a model around employeeSchema
+const EmployeeData = mongoose.model(
+"EmployeeData", employeeSchema);
+
+// Fetching all the employees
+app.get("/employees", (req, res) => {
+	EmployeeData.find((err, foundEmployees) => {
+		if (!err) {
+			res.send(foundEmployees)
+		} else {
+			res.send(err);
+		}
+	});
 });
 
-app.post('/login', (req, res) => {
-  // Insert Login Code Here
-  let username = req.body.username;
-  let password = req.body.password;
-  let code = req.body.code;
-  res.send(`Username: ${username} Password: ${password} Code: ${code}`);
+// Posting a new employee
+app.post("/employees", (req, res) => {
+	const newEmployee = new EmployeeData({
+		employee_name: req.body.employee_name,
+		employee_department: req.body.employee_department,
+		employee_salary: req.body.employee_salary
+	});
+
+	// Saving the employee
+	newEmployee.save(function(err) {
+		if (!err) {
+			res.send("Successfully added a new employee.");
+		} else {
+			res.send(err);
+		}
+	});
 });
 
-app.use(cors()); //habilitando cors na nossa aplicacao
+// Fetching a specific employee
+app.get("/employees/:employee_name", function(req, res) {
 
-app.listen(3000); //execucao do servidor
+	EmployeeData.findOne({ employee_name: req.params.employee_name },
+						function(err, foundEmployees) {
+		if (foundEmployees) {
+			res.send(foundEmployees);
+		} else {
+			res.send("No employee matching that name was found.");
+		}
+	});
+});
+
+app.listen(3000, function() {
+	console.log("Server started on port 3000");
+});
